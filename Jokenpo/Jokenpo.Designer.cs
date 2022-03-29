1f677e7b438e3ef42fdca3dcf1e8f094270fe8a3
JonathasGomes <jonathas553@hotmail.com>
2022-03-29 08:21:29 -0300

    feature: size and images

--- Jokenpo/Jokenpo.Designer.cs
+++ Jokenpo/Jokenpo.Designer.cs
@@ -29,13 +29,25 @@ namespace Jokenpo
         /// </summary>
         private void InitializeComponent()
         {
+            this.button1 = new System.Windows.Forms.Button();
             this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(214, 191);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(64, 64);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
             // 
             // Jokenpo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(567, 359);
+            this.ClientSize = new System.Drawing.Size(436, 512);
+            this.Controls.Add(this.button1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Jokenpo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Jokenpo";
@@ -44,6 +56,8 @@ namespace Jokenpo
         }
 
         #endregion
+
+        private System.Windows.Forms.Button button1;
     }
 }
 
