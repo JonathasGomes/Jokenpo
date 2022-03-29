f911fc4a39b5a602f801dc6c2dcfecb6d01a11f8
JonathasGomes <jonathas553@hotmail.com>
2022-03-29 09:47:19 -0300

    feature: player choice

--- Jokenpo/Jokenpo.Designer.cs
+++ Jokenpo/Jokenpo.Designer.cs
@@ -29,80 +29,130 @@ namespace Jokenpo
         /// </summary>
         private void InitializeComponent()
         {
-            this.button1 = new System.Windows.Forms.Button();
-            this.pictureBox1 = new System.Windows.Forms.PictureBox();
-            this.pictureBox2 = new System.Windows.Forms.PictureBox();
-            this.pictureBox3 = new System.Windows.Forms.PictureBox();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            this.Paper = new System.Windows.Forms.PictureBox();
+            this.pBJogador = new System.Windows.Forms.PictureBox();
+            this.pBIA = new System.Windows.Forms.PictureBox();
+            this.Scissors = new System.Windows.Forms.PictureBox();
+            this.Rock = new System.Windows.Forms.PictureBox();
+            this.lblCPU = new System.Windows.Forms.Label();
+            this.lblPlayer = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.Paper)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBJogador)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBIA)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Scissors)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rock)).BeginInit();
             this.SuspendLayout();
             // 
-            // button1
+            // Paper
             // 
-            this.button1.Location = new System.Drawing.Point(152, 330);
-            this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(64, 64);
-            this.button1.TabIndex = 0;
-            this.button1.Text = "button1";
-            this.button1.UseVisualStyleBackColor = true;
+            this.Paper.BackColor = System.Drawing.SystemColors.GrayText;
+            this.Paper.Location = new System.Drawing.Point(167, 402);
+            this.Paper.Name = "Paper";
+            this.Paper.Size = new System.Drawing.Size(64, 64);
+            this.Paper.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Paper.TabIndex = 1;
+            this.Paper.TabStop = false;
+            this.Paper.Tag = "Paper";
+            this.Paper.Click += new System.EventHandler(this.Choices_Click);
             // 
-            // pictureBox1
+            // pBJogador
             // 
-            this.pictureBox1.Image = global::Jokenpo.Properties.Resources.scissors;
-            this.pictureBox1.Location = new System.Drawing.Point(81, 96);
-            this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox1.TabIndex = 1;
-            this.pictureBox1.TabStop = false;
+            this.pBJogador.BackColor = System.Drawing.SystemColors.ActiveCaption;
+            this.pBJogador.Location = new System.Drawing.Point(167, 255);
+            this.pBJogador.Name = "pBJogador";
+            this.pBJogador.Size = new System.Drawing.Size(64, 64);
+            this.pBJogador.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBJogador.TabIndex = 2;
+            this.pBJogador.TabStop = false;
             // 
-            // pictureBox2
+            // pBIA
             // 
-            this.pictureBox2.Image = global::Jokenpo.Properties.Resources.rock;
-            this.pictureBox2.Location = new System.Drawing.Point(174, 96);
-            this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox2.TabIndex = 2;
-            this.pictureBox2.TabStop = false;
+            this.pBIA.BackColor = System.Drawing.SystemColors.Highlight;
+            this.pBIA.Location = new System.Drawing.Point(167, 64);
+            this.pBIA.Name = "pBIA";
+            this.pBIA.Size = new System.Drawing.Size(64, 64);
+            this.pBIA.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.pBIA.TabIndex = 3;
+            this.pBIA.TabStop = false;
             // 
-            // pictureBox3
+            // Scissors
             // 
-            this.pictureBox3.Image = global::Jokenpo.Properties.Resources.paper;
-            this.pictureBox3.Location = new System.Drawing.Point(273, 96);
-            this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(64, 64);
-            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
-            this.pictureBox3.TabIndex = 3;
-            this.pictureBox3.TabStop = false;
+            this.Scissors.BackColor = System.Drawing.SystemColors.InactiveCaptionText;
+            this.Scissors.Location = new System.Drawing.Point(280, 402);
+            this.Scissors.Name = "Scissors";
+            this.Scissors.Size = new System.Drawing.Size(64, 64);
+            this.Scissors.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Scissors.TabIndex = 5;
+            this.Scissors.TabStop = false;
+            this.Scissors.Tag = "Scissor";
+            this.Scissors.Click += new System.EventHandler(this.Choices_Click);
+            // 
+            // Rock
+            // 
+            this.Rock.BackColor = System.Drawing.SystemColors.ButtonHighlight;
+            this.Rock.Location = new System.Drawing.Point(74, 402);
+            this.Rock.Name = "Rock";
+            this.Rock.Size = new System.Drawing.Size(64, 64);
+            this.Rock.SizeMode = System.Windows.Forms.PictureBoxSizeMode.Zoom;
+            this.Rock.TabIndex = 4;
+            this.Rock.TabStop = false;
+            this.Rock.Tag = "Rock";
+            this.Rock.Click += new System.EventHandler(this.Choices_Click);
+            // 
+            // lblCPU
+            // 
+            this.lblCPU.AutoSize = true;
+            this.lblCPU.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCPU.Location = new System.Drawing.Point(29, 32);
+            this.lblCPU.Name = "lblCPU";
+            this.lblCPU.Size = new System.Drawing.Size(59, 25);
+            this.lblCPU.TabIndex = 6;
+            this.lblCPU.Text = "CPU";
+            // 
+            // lblPlayer
+            // 
+            this.lblPlayer.AutoSize = true;
+            this.lblPlayer.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPlayer.Location = new System.Drawing.Point(26, 351);
+            this.lblPlayer.Name = "lblPlayer";
+            this.lblPlayer.Size = new System.Drawing.Size(97, 31);
+            this.lblPlayer.TabIndex = 7;
+            this.lblPlayer.Text = "Player";
             // 
             // Jokenpo
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(436, 512);
-            this.Controls.Add(this.pictureBox3);
-            this.Controls.Add(this.pictureBox2);
-            this.Controls.Add(this.pictureBox1);
-            this.Controls.Add(this.button1);
+            this.Controls.Add(this.lblPlayer);
+            this.Controls.Add(this.lblCPU);
+            this.Controls.Add(this.Scissors);
+            this.Controls.Add(this.Rock);
+            this.Controls.Add(this.pBIA);
+            this.Controls.Add(this.pBJogador);
+            this.Controls.Add(this.Paper);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None;
             this.Name = "Jokenpo";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Jokenpo";
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
-            ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Paper)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBJogador)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pBIA)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Scissors)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.Rock)).EndInit();
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
-
-        private System.Windows.Forms.Button button1;
-        private System.Windows.Forms.PictureBox pictureBox1;
-        private System.Windows.Forms.PictureBox pictureBox2;
-        private System.Windows.Forms.PictureBox pictureBox3;
+        private System.Windows.Forms.PictureBox Paper;
+        private System.Windows.Forms.PictureBox pBJogador;
+        private System.Windows.Forms.PictureBox pBIA;
+        private System.Windows.Forms.PictureBox Scissors;
+        private System.Windows.Forms.PictureBox Rock;
+        private System.Windows.Forms.Label lblCPU;
+        private System.Windows.Forms.Label lblPlayer;
     }
 }
 
--- Jokenpo/Jokenpo.cs
+++ Jokenpo/Jokenpo.cs
@@ -16,5 +16,37 @@ namespace Jokenpo
         {
             InitializeComponent();
         }
+
+        int PlayerScore, CPUScore;
+        enum Choices { Rock, Paper, Scissor}
+        Choices[] choices = { Choices.Rock, Choices.Paper, Choices.Scissor };
+        Choices PlayerChoice, CPUChoice;
+        bool CanPlay = true;
+
+        private void Choices_Click(object sender, EventArgs e)
+        {
+            PictureBox pic = (PictureBox)sender;
+            if (CanPlay)
+            {
+                switch (pic.Tag.ToString())
+                {
+                    case "Rock":
+                        PlayerChoice = Choices.Rock;
+                        pBJogador.Image = Properties.Resources.rock;
+                        break;
+                    case "Paper":
+                        PlayerChoice = Choices.Paper;
+                        pBJogador.Image = Properties.Resources.paper;
+                        break;
+                    case "Scissor":
+                        PlayerChoice = Choices.Paper;
+                        pBJogador.Image = Properties.Resources.scissors;
+                        break;
+                }
+                CanPlay = false;
+
+            }
+
+        }
     }
 }
